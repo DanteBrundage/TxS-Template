a36e31ebcf83c83dd9db5ab159ee18086919fe8e
Radames Ajna <radamajna@gmail.com>
2018-08-10 17:39:12 -0700

    get album conver image from spotify

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // drake album picture https://i.scdn.co/image/012ecd119617ac24ab56620ace4b81735b172686
 //album URI
 // songURI spotify:track:2G7V7zsVDxg1yRsu7Ew9RJ
+//spotify:track:2i8nLoRrUgY0zVtsDxCooV
 
 const gifs = [
   'https://i.giphy.com/media/BCXMSiVZeo8xy/giphy.gif',
@@ -14,16 +15,21 @@ const gifs = [
   'https://i.giphy.com/media/1TJB4TPjtaEJq/giphy.gif',
   'https://i.giphy.com/media/xe9csf50g4SqY/giphy.gif',
   'https://i.giphy.com/media/l0HlOWpCtvk3ImJpe/giphy.gif'
-]
+];
 // spotify:album:7yf9ZJyHRbp8sHtpDKfPpt
+
+setAlbumCover();
+
+async function setAlbumCover(){
+  var aa = await getAlbumCover('spotify:track:2i8nLoRrUgY0zVtsDxCooV');
+  document.querySelector('.box-image').style.backgroundImage = `url(${aa})`;
+}
+
+
 document.querySelector('#range').addEventListener('change', (e) => {
-  console.log(e.target.value)
   document.querySelector('.score').innerHTML = e.target.value / 10
   document.querySelector('.box-image').style.backgroundImage = `url(${gifs[Math.floor((e.target.value)/10)]})`;
 })
-
-console.log(getAlbumCover('spotify:album:3X8Nrq4TvSd54ymTiXYcD6'))
-
 
 
 
@@ -33,10 +39,10 @@ function getSpotifySrc(song) {
 }
 
 
-function getAlbumCover(song) {
+async function getAlbumCover(song) {
   //return the Image for a specific song ID
   const songCode = song.split(':')[2];
-  const data = fetch(`https://cors-anywhere.herokuapp.com/https://embed.spotify.com/oembed?url=http://open.spotify.com/track/${songCode}`).
-                     then(r => r.json());
-  return data;
+  const data = await fetch(`https://cors-anywhere.herokuapp.com/https://embed.spotify.com/oembed?url=http://open.spotify.com/track/${songCode}`)
+    .then(r => r.json());
+  return data.thumbnail_url;
 }
