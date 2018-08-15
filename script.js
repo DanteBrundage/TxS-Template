105d95c04fa101f38ef6e17de11138dc85c4ab50
DanteBrundage <danteb280@gmail.com>
2018-08-14 18:01:15 -0700

    slider

--- script.js
+++ script.js
@@ -16,17 +16,18 @@
 
 // those are variables selecting main elements on the page
 
-var resultBox = document.getElementById('result')
-var titleBox = document.getElementById('title')
-var gifPanel = document.getElementById('gif-panel')
-var mainImage = document.getElementById('main-image')
-var youImage = document.getElementById('you-image')
-var spotifyImage = document.getElementById('spotify-image')
-var yrImage = document.getElementById('yr-image')
-var youScore = document.getElementById('you-score')
-var spotifyScore = document.getElementById('spotify-score')
-var yrScore = document.getElementById('yr-score')
-var resultButton = document.getElementById('result-button')
+var resultBox = document.getElementById('result');
+var titleBox = document.getElementById('title');
+var gifPanel = document.getElementById('gif-panel');
+var mainImage = document.getElementById('main-image');
+var youImage = document.getElementById('you-image');
+var spotifyImage = document.getElementById('spotify-image');
+var yrImage = document.getElementById('yr-image');
+var youScore = document.getElementById('you-score');
+var spotifyScore = document.getElementById('spotify-score');
+var yrScore = document.getElementById('yr-score');
+var resultButton = document.getElementById('result-button');
+var pleaseVote = document.getElementById('vote-box');
 var gifs = [
   "url(https://i.giphy.com/media/BCXMSiVZeo8xy/giphy.gif)",
   "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)",
@@ -52,20 +53,22 @@ var colors = [
   '#7ff4bd',
   '#dd11b5'
 ];
+var slider = document.getElementById("range");
 // this is how you replace the image from the background
 //mainImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
 
 // this is how we hide the resultBox element
 resultBox.hidden = true;
 resultButton.hidden = true;
+pleaseVote.hidden = false;
 
 // this is how you create a function
 function testClickFunction() {
-  alert('clickedddd')
+  alert('clickedddd');
 }
 
 function buttons() {
-  resultButton.hidden = false
+  resultButton.hidden = false;
 }
 
 // Activity today will be
@@ -83,41 +86,22 @@ function gifFunction(rate) {
 // 3. Add the functions to the correpondent HTML elements, using the onclick="functionName()" attribute
 // 4. Create a function to process the click on Compare button
 
+function sliderChange()
+{
+  userRating = slider.value;
+  console.log(userRating);
+  mainImage.style.backgroundImage = gifs[userRating - 1];
+  gifPanel.style.background = colors[userRating - 1];
+  resultButton.hidden = false;
+  pleaseVote.hidden = true;
+}
 
 function onClick() {
   youScore.innerHTML = userRating;
   gifPanel.hidden = true;
   resultBox.hidden = false;
-  if (userRating === 1) {
-    youImage.style.backgroundImage = "url(https://i.giphy.com/media/BCXMSiVZeo8xy/giphy.gif)";
-  }
-  if (userRating === 2) {
-    youImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)";
-  }
-  if (userRating === 3) {
-    youImage.style.backgroundImage = "url(https://i.giphy.com/media/3o6Zt5nSYQa0czSmnm/giphy.gif)";
-  }
-  if (userRating === 4) {
-    youImage.style.backgroundImage = "url(https://i.giphy.com/media/2tKCVxXIkBgqUZGDJb/giphy.gif)";
-  }
-  if (userRating === 5) {
-    youImage.style.backgroundImage = "url(https://i.giphy.com/media/KtfjlVSFI8EVO/giphy.gif)";
-  }
-  if (userRating === 6) {
-    youImage.style.backgroundImage = "url(https://i.giphy.com/media/xUPGGDgB1ZlUduRov6/giphy.gif)";
-  }
-  if (userRating === 7) {
-    youImage.style.backgroundImage = "url(https://i.giphy.com/media/wAxlCmeX1ri1y/giphy.gif)";
-  }
-  if (userRating === 8) {
-    youImage.style.backgroundImage = "url(https://i.giphy.com/media/MhHcCN6PoTdL2/giphy.gif)";
-  }
-  if (userRating === 9) {
-    youImage.style.backgroundImage = "url(https://i.giphy.com/media/1TJB4TPjtaEJq/giphy.gif)";
-  }
-  if (userRating === 10) {
-    youImage.style.backgroundImage = "url(https://i.giphy.com/media/xe9csf50g4SqY/giphy.gif)";
-  }
+  youImage.style.backgroundImage = gifs[userRating - 1];
+
 }
 
 
