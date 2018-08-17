bc28a26652e74684738126c61dd45446af7621d6
DanteBrundage <danteb280@gmail.com>
2018-08-16 17:42:55 -0700

    finish button, spotify gifs and ratings

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ var youScore = document.getElementById('you-score');
 var spotifyScore = document.getElementById('spotify-score');
 var yrScore = document.getElementById('yr-score');
 var resultButton = document.getElementById('result-button');
+var nextSongButton = document.getElementById('next-song');
+var finishButton = document.getElementById('finish');
 var pleaseVote = document.getElementById('vote-box');
 var gifs = [
   "url(https://i.giphy.com/media/BCXMSiVZeo8xy/giphy.gif)",
@@ -40,7 +42,12 @@ var gifs = [
   "url(https://i.giphy.com/media/1TJB4TPjtaEJq/giphy.gif)",
   "url(https://i.giphy.com/media/xe9csf50g4SqY/giphy.gif)"
 ];
+var songs = [
 
+  "spotify:track:0KhM1c2bkqEAdxydjmZQPs", //First light hidden orchestra
+  "spotify:track:2vmGgEn95r2rNAzN5v3h4X", // Homebody
+  "spotify:track:0NVNmybggOg7HqjcxCkvpq"
+];
 var colors = [
   '#4286f4',
   '#4286f4',
@@ -53,8 +60,18 @@ var colors = [
   '#7ff4bd',
   '#dd11b5'
 ];
+var spotifyScores = [2,4,8];
+var userRatings = [];
+var iframeElement = document.querySelector("#song-iframe"); //select Iframe
+
 var slider = document.getElementById("range");
-// this is how you replace the image from the background
+var progress = 0;
+
+
+iframeElement.src = getSpotifySrc(songs[progress])
+setAlbumCover(songs[progress] , mainImage)
+
+
 //mainImage.style.backgroundImage = "url(https://i.giphy.com/media/1d5KHhOA1oTpX7ROOi/giphy.gif)"
 
 // this is how we hide the resultBox element
@@ -89,21 +106,81 @@ function gifFunction(rate) {
 function sliderChange()
 {
   userRating = slider.value;
-  console.log(userRating);
   mainImage.style.backgroundImage = gifs[userRating - 1];
   gifPanel.style.background = colors[userRating - 1];
   resultButton.hidden = false;
   pleaseVote.hidden = true;
 }
 
-function onClick() {
+function onClick(page) {
+  if ( page == "results") {
+  userRatings.push(slider.value);
+  spotifyScore.innerHTML = spotifyScores[progress];
   youScore.innerHTML = userRating;
   gifPanel.hidden = true;
   resultBox.hidden = false;
+  if(userRatings.length == spotifyScores.length){
+    nextSongButton.hidden = true;
+    finishButton.hidden = false;
+  }
+  else
+  {
+    nextSongButton.hidden = false;
+    finishButton.hidden = true;
+  }
   youImage.style.backgroundImage = gifs[userRating - 1];
+  spotifyImage.style.backgroundImage = gifs[spotifyScores[progress] - 1];
+  progress += 1;
+  console.log("user has completed: " + progress + " songs");
+  console.log(userRatings);
 
+  }
+  else
+  {
+    userRating = null;
+    resultButton.hidden = true;
+    gifPanel.style.background = 'white';
+    youScore.innerHTML = userRating;
+    gifPanel.hidden = false;
+    resultBox.hidden = true;
+    iframeElement.src = getSpotifySrc(songs[progress]);
+    setAlbumCover(songs[progress] , mainImage);
+
+  }
 }
 
+
+
+
+//you have to select the iframe element/
+//copy the next line to the top of your code
+
+
+//this is how you set the new src for your Spotify oembed/
+
+
+//this is how you set a new album cover
+
+
+//copy this to your file once at the end of your scripts.js
+//Special Functions
+function getSpotifySrc(song) {
+  const songCode = song.split(':')[2];
+  return `https://open.spotify.com/embed/track/${songCode}`;
+}
+
+async function setAlbumCover(song, element){
+  var aa = await getAlbumCover(song);
+  element.style.backgroundImage = `url(${aa})`;
+}
+
+async function getAlbumCover(song) {
+  //return the Image for a specific song ID
+  const songCode = song.split(':')[2];
+  const data = await fetch(`https://cors-anywhere.herokuapp.com/https://embed.spotify.com/oembed?url=http://open.spotify.com/track/${songCode}`)
+    .then(r => r.json());
+  return data.thumbnail_url;
+}
 
 
 // 5. The function should, hide the element voteBox and show resultBox
